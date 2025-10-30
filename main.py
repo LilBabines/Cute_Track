4ffe5e05302c7512bed005888090e76ac3c6f39d
LilBabines <augusteverdier@orange.fr>
2025-10-30 13:35:55 +0100

    set up

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 
 from PyQt6 import QtWidgets
-from src.qt_windows import OBB_VideoPlayer
+from src.qt_windows import OBB_VideoPlayer,BaseVideoPlayer
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    w = OBB_VideoPlayer()
+    w = BaseVideoPlayer()
     w.show()
     sys.exit(app.exec())
 
